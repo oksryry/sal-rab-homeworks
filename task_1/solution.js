2a1f8096b39868afbca92880e68b3615247fa0e5
oksry <oks.ryzh@gmail.com>
2021-12-07 16:50:24 +0300

    Task 1 is done I hope

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,7 +4,7 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
+ let message = productName "за" productPrice "теперь в корзине!";   // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
@@ -12,7 +12,7 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+  console.log(message); // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
 
