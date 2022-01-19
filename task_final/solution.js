808d99c690b47c4c3d9e1a60adbce166eee01c65
oksry <oks.ryzh@gmail.com>
2022-01-19 23:49:35 +0300

    Добавила финальную версию задания, автотесты пройдены

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,19 +35,21 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
-
+    
     let countOfGoods = goods.length;
+  
+        for (let i = 0; i < countOfGoods; i += 1){ 
+            data.goods[i] = {title: goods[i].title, count: goods[i].count};
+    };
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    data.client = `${name} ${phone}`;
+ 
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`; 
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
+    
 }
